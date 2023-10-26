3ffa1d32074b6576626efe4cb64f0792aa7c61ae
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:03:43 +0200

    test of SQL export

--- SMCollection/schema.sql
+++ SMCollection/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SMCollection of the subject dataModel.SocialMedia for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SMCollection_type AS ENUM ('SMCollection');
-CREATE TABLE SMCollection (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, groupedAt TIMESTAMP, hasAnalysis JSON, hasPosts JSON, name TEXT, owner JSON, source TEXT, type SMCollection_type);
+CREATE TABLE SMCollection (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, groupedAt TIMESTAMP, hasAnalysis JSON, hasPosts JSON, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type SMCollection_type);
